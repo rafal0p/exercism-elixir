209c30d32e38cfffe9b0f4124f2fdcf0ade7fbc0
Rafal Podlipny <rafal0p@outlook.com>
2019-04-14 10:02:58 +0200

    nucleotide-count 1

--- nucleotide-count/nucleotide_count.exs
+++ nucleotide-count/nucleotide_count.exs
@@ -14,6 +14,7 @@ defmodule NucleotideCount do
   """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
+    length(Enum.filter(strand, &(nucleotide == &1)))
   end
 
   @doc """
@@ -26,5 +27,11 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
+    %{
+      ?A => count(strand, ?A),
+      ?T => count(strand, ?T),
+      ?C => count(strand, ?C),
+      ?G => count(strand, ?G)
+    }
   end
 end
