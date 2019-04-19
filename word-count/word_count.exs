01992cd056e363a7eb4b4a90dda77b8d5aa1a12c
Rafal Podlipny <rafal0p@outlook.com>
2019-04-19 18:57:12 +0200

    word-count 1
    ugly as hell

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,5 +6,44 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    do_count(String.graphemes(sentence <> " "))
+  end
+
+  defp do_count(_, acc \\ %{}, word \\ "")
+
+  defp do_count([], acc, _) do
+    acc
+  end
+
+  defp do_count([" " | tail], acc, "") do
+    do_count(tail, acc, "")
+  end
+
+  defp do_count([" " | tail], acc, word) do
+    do_count(tail, Map.put(acc, word, Map.get(acc, word, 0) + 1), "")
+  end
+
+  defp do_count(["_" | tail], acc, word) do
+    do_count(tail, Map.put(acc, word, Map.get(acc, word, 0) + 1), "")
+  end
+
+  defp do_count([character | tail], acc, word) do
+    if word_creating?(character) do
+      do_count(tail, acc, word <> String.downcase(character))
+    else
+      do_count(tail, acc, word)
+    end
+  end
+
+  defp word_creating?(character) do
+    isLetter(character) || isDigit(character) || character == "-"
+  end
+
+  defp isDigit(s) do
+    Enum.member?(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], s)
+  end
+
+  defp isLetter(s) do
+    !(String.upcase(s) == String.downcase(s))
   end
 end
