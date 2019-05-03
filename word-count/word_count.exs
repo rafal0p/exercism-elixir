c470ff39a3a873d8e5a4c3db85e134933f834c74
Rafal Podlipny <rafal0p@outlook.com>
2019-05-03 08:39:54 +0200

    word-count 5
    using Regex.split/2

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,37 +6,11 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    do_count(String.graphemes(sentence <> " "))
-  end
-
-  defp do_count(_, acc \\ %{}, word \\ "")
-
-  defp do_count([], acc, _), do: acc
-
-  defp do_count([character | tail], acc, word) do
-    case {character, word} do
-      {" ", ""} ->
-        do_count(tail, acc, "")
-      {character, word} when character in [" ", "_"] ->
-        do_count(tail, Map.update(acc, word, 1, &(&1 + 1)), "")
-      {character, word} ->
-        if word_creating?(character) do
-          do_count(tail, acc, word <> String.downcase(character))
-        else
-          do_count(tail, acc, word)
-        end
-    end
-  end
-
-  defp word_creating?(character) do
-    letter?(character) || digit?(character) || character == "-"
-  end
-
-  defp digit?(s) do
-    Enum.member?(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], s)
-  end
-
-  defp letter?(s) do
-    !(String.upcase(s) == String.downcase(s))
+    Regex.split(~r/[ !&@$%^:,_]/, String.downcase(sentence))
+    |> Enum.filter(fn word -> word != "" end)
+    |> Enum.reduce(
+         %{},
+         fn word, acc -> Map.update(acc, word, 1, &(&1 + 1)) end
+       )
   end
 end
