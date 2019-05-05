d6619d23a8313e748cfbfeca03430cb6f168b422
Rafal Podlipny <rafal0p@outlook.com>
2019-05-05 13:32:45 +0200

    bob 1

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,7 +1,27 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      shout?(input) && question?(input) -> "Calm down, I know what I'm doing!"
+      shout?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
+      silence?(input) -> "Fine. Be that way!"
+      true -> "Whatever."
     end
+  end
+
+  defp shout?(input) do
+    String.upcase(input) == input && at_least_one_letter(input)
+  end
+
+  defp at_least_one_letter(input) do
+    String.match?(input, ~r/\p{L}+/u)
+  end
+
+  defp question?(input) do
+    String.last(input) == "?"
+  end
+
+  defp silence?(input) do
+    String.trim(input) == ""
   end
 end
