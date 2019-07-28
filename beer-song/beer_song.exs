8c8ca7e9b0548727be33b33d0a8290013730079b
Rafał Podlipny <rafal0p@outlook.com>
2019-07-28 19:24:27 +0200

    beer-song 2
    make it right

--- beer-song/beer_song.exs
+++ beer-song/beer_song.exs
@@ -3,39 +3,28 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(0) do
-    """
-    No more bottles of beer on the wall, no more bottles of beer.
-    Go to the store and buy some more, 99 bottles of beer on the wall.
-    """
-  end
-
-  def verse(1) do
-    """
-    1 bottle of beer on the wall, 1 bottle of beer.
-    Take it down and pass it around, no more bottles of beer on the wall.
-    """
-  end
-
-  def verse(2) do
-    """
-    2 bottles of beer on the wall, 2 bottles of beer.
-    Take one down and pass it around, 1 bottle of beer on the wall.
-    """
-  end
-
   def verse(number) do
     """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    #{String.capitalize(bottles(number))} of beer on the wall, #{bottles(number)} of beer.
+    #{passing(number)}, #{bottles_after_passing(number)} of beer on the wall.
     """
   end
+
+  defp bottles(0), do: "no more bottles"
+  defp bottles(1), do: "1 bottle"
+  defp bottles(number), do: "#{number} bottles"
+
+  defp bottles_after_passing(number), do: bottles(Integer.mod(number - 1, 100))
+
+  defp passing(0), do: "Go to the store and buy some more"
+  defp passing(1), do: "Take it down and pass it around"
+  defp passing(number), do: "Take one down and pass it around"
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range \\ (99 .. 0)) do
+  def lyrics(range \\ (99..0)) do
     Enum.map(range, &verse/1)
     |> Enum.join("\n")
   end
